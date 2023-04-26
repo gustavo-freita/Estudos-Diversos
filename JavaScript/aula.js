c6840306abda76036cd6be03914c1a1c528e2676
Gustavo Freitas <gustavofreitas_silva@hotmail.com>
2023-04-26 20:35:27 -0300

    :pencil:build: termino dos exercicios de arrays

--- JavaScript/aula.js
+++ JavaScript/aula.js
@@ -176,6 +176,20 @@ const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
 console.log(soma / notas.length); */ 
 
 
-for (let i = 0; i < 10; i++) {  
-    console.log(i);
+const notas = [];
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+notas.push(8);
+
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma = soma + nota;
 }
+const media = soma / notas.length;
+console.log(media);
