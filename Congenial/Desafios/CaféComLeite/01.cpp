cdefa9d2b976943d310caca6a53e23f1e17d64c1
Gustavo Freitas <gustavofreitas_silva@hotmail.com>
2024-05-27 21:48:34 -0300

    Adicionando estudos de Lógica de Programação com C++

---
+++ Congenial/Desafios/CaféComLeite/01.cpp
@@ -0,0 +1,24 @@
+/*  Hello World
+ Mostrar detalhes
+Hello World é uma frase famosa que todo programador usa quando está aprendendo uma linguagem de programação.
+
+Tarefa
+Faça um programa que imprima "Hello World"
+
+Entrada
+Seu programa não receberá nenhuma entrada.
+
+Saída
+Seu programa deve imprimir "Hello World" sem aspas e com uma quebra de linha no final. */
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  cout << "Hello World" << endl;
+  
+  return 0;
+}
---
+++ Congenial/Desafios/CaféComLeite/02.cpp
@@ -0,0 +1,30 @@
+/* Eco é a repetição de um som que se dá pela reflexão de uma onda sonora por uma superfície ou um objeto.
+
+Na programação, um "eco" é um programa que simplesmente repete na saída o que recebe na entrada.
+
+Tarefa
+Escreva um programa que solicita um número N  como entrada e o imprime imediatamente na saída padrão.
+
+Entrada
+A primeira e única linha de entrada contém um número inteiro  N.
+
+Saída
+Seu programa deve imprimir o mesmo número fornecido na entrada, sem alterações, seguido de uma quebra de linha.
+
+Exemplo
+Entrada	Saída
+5       5
+1       1
+9       9 */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n ;
+  cin >> n;
+
+  cout << n << endl;
+  return 0;
+}
